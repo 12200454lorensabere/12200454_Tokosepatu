27e80485f4e2e5976398ca61a617115055f7523d
12200085rimpi <rimpygabriellas27@gmail.com>
2021-12-10 22:47:34 +0700

    file migration

--- app/Database/Migrations/2021-12-10-142020_TransactionDetails.php
+++ app/Database/Migrations/2021-12-10-142020_TransactionDetails.php
@@ -4,15 +4,23 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class TransactionDetails extends Migration
+class Users extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addColumn('transaction_details',[
+            'merk',
+            'harga',
+            'jumlah',
+            'total',
+        ]);
+        $this->forge->addkey('transaction_details', true);
+        $this->forge->createTable('transaction_details');
+        
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('transaction_details');
     }
 }
