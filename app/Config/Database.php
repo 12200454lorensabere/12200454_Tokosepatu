f99b64d874c129f144deb47da87e2eadeaef1f53
12200085rimpi <rimpygabriellas27@gmail.com>
2021-12-12 23:26:11 +0700

    file migration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'nikesepatu',
+        'database' => 'transactions_details',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
