da59540e1b5a256e960f7ca8ec82e4a756b12d9b
branata12200608 <branatamariska@gmail.com>
2021-12-13 00:39:05 +0700

    file migration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'transactions_details',
+        'database' => 'nikesepatu',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
